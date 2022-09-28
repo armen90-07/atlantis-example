6733f120ee8841654330bf8fcf1a537cb6895f93
armen90-07 <114574078+armen90-07@users.noreply.github.com>
2022-09-28 16:37:33 +0400

    Create es2.tf

---
+++ es2.tf
@@ -0,0 +1,24 @@
+
+terraform {
+  required_version = ">= 0.14"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.29.0"
+    }
+  }
+}
+
+variable "ami" {
+  type = string
+  description = "ami id"
+  default = "ami-09042b2f6d07d164a"
+}
+
+resource "aws_instance" "my-instance" {
+  ami                    = var.ami  # ami-09042b2f6d07d164a
+  instance_type          = "t2.micro"
+  tags = {
+    Name = "my_custom_server1"
+  }
+}
