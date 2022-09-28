5497c965f9a376d61630a4b8c29d64e51c8e2774
armen90-07 <114574078+armen90-07@users.noreply.github.com>
2022-09-28 17:01:18 +0400

    Update es2.tf

--- es2.tf
+++ es2.tf
@@ -15,7 +15,7 @@ variable "ami" {
   default = "ami-00b696228b0185ffe"
 }
 
-resource "aws_instance" "my-instance1" {
+resource "aws_instance" "my-instance" {
   ami                    = var.ami  # ami-09042b2f6d07d164a
   instance_type          = "t2.micro"
   tags = {
